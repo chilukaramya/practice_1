713322f939b9ba198fa6c6e25fba80e2f8f3ab8f
chilukaramya <ramyachiluka11@gmail.com>
2025-09-10 14:30:03 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        // Example numbers to multiply
+        int num1 = 5;
+        int num2 = 7;
+
+        int result = num1 * num2;
+
+        System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
