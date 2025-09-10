a8f8caa0ad0987a3f21c2c31251336c5f3bcc7f0
chilukaramya <ramyachiluka11@gmail.com>
2025-09-10 14:29:37 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 20; // Second number
+        int sum = num1 + num2; // Addition operation
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
